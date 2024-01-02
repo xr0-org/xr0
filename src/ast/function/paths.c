87099679fd3df8323a6ed3c05816b64636ed6ff3
Xr0 Team <x@xr0.dev>
2024-01-02 08:16:35 +0000

    checkpoint

--- src/ast/function/paths.c
+++ src/ast/function/paths.c
@@ -30,17 +30,38 @@ paths_fromfunction(struct ast_function *f)
 	return arr;
 }
 
+static int
+findsel(struct ast_block *b);
+
+static struct ast_function *
+abstract_immediate_split(struct ast_function *f, int i, bool enter);
+
 static struct ast_function_arr *
 abstract_paths(struct ast_function *f)
 {
 	struct ast_function_arr *arr = ast_function_arr_create();
-	/* TODO */
-	ast_function_arr_append(arr, ast_function_copy(f));
+	int sel = findsel(ast_function_abstract(f));
+	if (sel != -1) {
+		struct ast_function *t_p = abstract_immediate_split(f, sel, true),
+				    *f_p = abstract_immediate_split(f, sel, false);
+
+		struct ast_function_arr *t_paths = abstract_paths(t_p),
+					*f_paths = abstract_paths(f_p);
+
+		ast_function_arr_appendrange(arr, ast_function_arr_copy(t_paths));
+		ast_function_arr_appendrange(arr, ast_function_arr_copy(f_paths));
+
+		ast_function_arr_destroy(f_paths);
+		ast_function_arr_destroy(t_paths);
+
+		ast_function_destroy(f_p);
+		ast_function_destroy(t_p);
+
+	} else {
+		ast_function_arr_append(arr, ast_function_copy(f));
+	}
 	return arr;
 }
-
-static int
-findsel(struct ast_function *f);
 
 static struct ast_function *
 immediate_split(struct ast_function *f, int i, bool enter);
@@ -50,13 +71,13 @@ body_paths(struct ast_function *f)
 {
 	struct ast_function_arr *arr = ast_function_arr_create();
 
-	int sel = findsel(f);
+	int sel = findsel(ast_function_body(f));
 	if (sel != -1) {
 		struct ast_function *t_p = immediate_split(f, sel, true),
 				    *f_p = immediate_split(f, sel, false);
 
-		struct ast_function_arr *t_paths = paths_fromfunction(t_p),
-					*f_paths = paths_fromfunction(f_p);
+		struct ast_function_arr *t_paths = body_paths(t_p),
+					*f_paths = body_paths(f_p);
 
 		ast_function_arr_appendrange(arr, ast_function_arr_copy(t_paths));
 		ast_function_arr_appendrange(arr, ast_function_arr_copy(f_paths));
@@ -74,19 +95,15 @@ body_paths(struct ast_function *f)
 }
 
 static int
-findsel(struct ast_function *f)
+findsel(struct ast_block *b)
 {
-	assert(!ast_function_isaxiom(f));
-
-	struct ast_block *body = ast_function_body(f);
-	struct ast_stmt **stmt = ast_block_stmts(body);
-	int nstmts = ast_block_nstmts(body);
+	struct ast_stmt **stmt = ast_block_stmts(b);
+	int nstmts = ast_block_nstmts(b);
 	for (int i = 0; i < nstmts; i++) {
 		if (ast_stmt_isselection(stmt[i])) {
 			return i;
 		}
 	}
-
 	return -1;
 }
 
@@ -98,6 +115,34 @@ block_withassumption(struct ast_block *b, struct ast_expr *assumption);
 
 static struct ast_block *
 split_block_index(struct ast_block *b, int split_index, bool enter);
+
+static struct ast_function *
+abstract_immediate_split(struct ast_function *f, int split_index, bool enter)
+{
+	struct ast_block *abs = ast_function_abstract(f),
+			 *body = ast_function_body(f);
+
+	int nparam = ast_function_nparams(f);
+	struct ast_variable **params = ast_function_params(f);
+
+	/* TODO: check that cond is accessible in both abstract and body */
+	struct ast_expr *assumption = ast_expr_inverted_copy(
+		ast_stmt_sel_cond(ast_block_stmts(abs)[split_index]), !enter
+	);
+
+	return ast_function_create(
+		false,
+		ast_type_copy(ast_function_type(f)),
+		split_name(ast_function_name(f), assumption),
+		ast_function_nparams(f),
+		ast_variables_copy(nparam, params),
+		block_withassumption(
+			split_block_index(abs, split_index, enter),
+			assumption
+		),
+		ast_block_copy(body)
+	);
+}
 
 static struct ast_function *
 immediate_split(struct ast_function *f, int split_index, bool enter)
