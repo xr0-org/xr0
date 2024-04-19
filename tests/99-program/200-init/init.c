e8452c60b86f9b710ba5c992dc1a5261e925d38e
Amisi Kiarie <a@xr0.dev>
2024-04-19 07:56:37 +0300

    checkpoint

---
+++ tests/99-program/200-init/init.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int
+main()
+{
+	int x;
+	printf("x: %d\n", x);
+}
+
+struct list {
+	int x;
+	struct list *next;
+};
+
+void
+foo()
+{
+	struct point { int x; int y; } p;
+	struct point p2;
+
+	struct man { char *name; int age; };
+
+	struct man john;
+}
