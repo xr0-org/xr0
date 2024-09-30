e585d1576b28d80844b9e68b4cf56df2062a0af1
Claude Betz <betz@xr0.dev>
2024-09-30 11:27:40 +0000

    rename but path of error messages now appends "tests/0v"

--- tests/0db/00-basic/test.c
+++ tests/0db/00-basic/test.c
@@ -1,14 +1,24 @@
+#ifdef XR0
+
 void
 modify0(int *q, int x) ~ [
 	setup: q = .clump(1);
 	if (x) {
 		*q = 1;
 	}
-] {
+];
+
+#endif
+
+void
+modify0(int *q, int x)
+{
 	if (x) {
 		*q = 1;
 	}
 }
+
+#ifdef XR0
 
 void
 modify1(int *q, int x) ~ [
@@ -16,24 +26,49 @@ modify1(int *q, int x) ~ [
 		setup: q = .clump(1);
 		*q = 2;
 	}
-] {
+];
+
+#endif
+
+void
+modify1(int *q, int x)
+{
 	if (x) {
 		*q = 2;
 	}
 }
 
-int *
+int
 main()
 {
 	int p;
 	p = 0;
+
+	#ifdef XR0
 	~ [ p == 0; ]
+	#endif
+
 	modify0(&p, 0);
+
+	#ifdef XR0
 	~ [ p == 0; ]
+	#endif
+
 	modify0(&p, 1);
+
+	#ifdef XR0
 	~ [ p == 1; ]
+	#endif
+
 	modify1(&p, 0);
+
+	#ifdef XR0
 	~ [ p == 1; ]
+	#endif
+
 	modify1(&p, 1);
+	
+	#ifdef XR0
 	~ [ p == 2; ]
+	#endif
 }
